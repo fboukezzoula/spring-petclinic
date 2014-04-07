903b449d2339ba35ed158c8dd634f24bc629c50c
alm-finaxys <groupe-alm@finaxys.com>
2014-04-07 17:03:38 +0200

    code break

--- src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
@@ -21,7 +21,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
-
+import totro;
 /**
  * Simple JavaBean domain object with an id property. Used as a base class for objects needing this property.
  *
